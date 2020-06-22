9d61491afa7cae37b019c273320620ac7a4ba5e7
unknown <lima.renan96@gmail.com>
2020-06-22 14:32:49 -0300

    list and create products

--- products/urls.py
+++ products/urls.py
@@ -8,4 +8,5 @@ app_name='products'
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
+    path('create/', views.product_create, name='product_create'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # models
 from products.models import Product
 
 
 def product_list(request):
-    pass
+    products = Product.objects.all()
+    context = {
+        'products':products,
+    }
+    return render(request, 'product_list.html', context)
+
+
+def product_create(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        price = request.POST['price']
+        category = request.POST['category']
+
+        Product.objects.create(
+            name=name,
+            price=price,
+            category=category,
+        )
+        return redirect('products:product_list')
+    return render(request, 'product_create.html')
