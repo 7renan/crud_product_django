1b4fb04fe11e4e8a12be5d43661f86d5ec803514
unknown <lima.renan96@gmail.com>
2020-06-22 17:07:39 -0300

    update and delete products

--- products/urls.py
+++ products/urls.py
@@ -9,4 +9,6 @@ app_name='products'
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('create/', views.product_create, name='product_create'),
+    path('update/<int:pk_product>/', views.product_update, name='product_update'),
+    path('delete/<int:pk_product>/', views.product_delete, name='product_delete'),
 ]
--- products/views.py
+++ products/views.py
@@ -25,3 +25,30 @@ def product_create(request):
         )
         return redirect('products:product_list')
     return render(request, 'product_create.html')
+
+
+def product_update(request, pk_product):
+    product = Product.objects.get(pk=pk_product)
+    context = {
+        'product':product,
+    }
+
+    if request.method == 'POST':
+        new_name = request.POST['name']
+        new_price = request.POST['price']
+        new_category = request.POST['category']
+
+        product.name = new_name
+        product.price = new_price
+        product.category = new_category
+        product.save()
+
+        return redirect('products:product_list')
+
+    return render(request, 'product_update.html', context)
+
+
+def product_delete(request, pk_product):
+    product = Product.objects.get(pk=pk_product)
+    product.delete()
+    return redirect('products:product_list')
